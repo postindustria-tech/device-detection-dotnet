a7731b413900ec8cba0017884460539677423b9d
Maksym Kucherov <maximus.the0@gmail.com>
2023-10-20 18:14:33 +0300

    Forward `$LASTEXITCODE` out of `run-performance-tests.ps1`

--- ci/run-performance-tests.ps1
+++ ci/run-performance-tests.ps1
@@ -10,8 +10,14 @@ param(
 
 . ./$RepoName/ci/run-performance-tests-console.ps1 -RepoName $RepoName -OrgName $OrgName -Name $Name -Configuration $Configuration -Arch $Arch
 
+if ($LASTEXITCODE -ne 0) {
+    exit $LASTEXITCODE
+}
+
 Write-Output ""
 Write-Output "===== CONSOLE / WEB DELIMITER ====="
 Write-Output ""
 
 . ./$RepoName/ci/run-performance-tests-web.ps1 -RepoName $RepoName -ProjectDir $ProjectDir -Name $Name -Configuration $Configuration -Arch $Arch
+
+exit $LASTEXITCODE
