ce433f81f644353e539645f9a0194ec67c3282c7
Maksym Kucherov <maximus.the0@gmail.com>
2023-10-23 11:31:38 +0300

    [TO BE REVERTED] Minimize the scope of CI runs.

--- ci/run-performance-tests.ps1
+++ ci/run-performance-tests.ps1
@@ -10,14 +10,14 @@ param(
 
 . ./$RepoName/ci/run-performance-tests-console.ps1 -RepoName $RepoName -OrgName $OrgName -Name $Name -Configuration $Configuration -Arch $Arch
 
-if ($LASTEXITCODE -ne 0) {
-    exit $LASTEXITCODE
-}
+# if ($LASTEXITCODE -ne 0) {
+#     exit $LASTEXITCODE
+# }
 
-Write-Output ""
-Write-Output "===== CONSOLE / WEB DELIMITER ====="
-Write-Output ""
+# Write-Output ""
+# Write-Output "===== CONSOLE / WEB DELIMITER ====="
+# Write-Output ""
 
-. ./$RepoName/ci/run-performance-tests-web.ps1 -RepoName $RepoName -ProjectDir $ProjectDir -Name $Name -Configuration $Configuration -Arch $Arch
+# . ./$RepoName/ci/run-performance-tests-web.ps1 -RepoName $RepoName -ProjectDir $ProjectDir -Name $Name -Configuration $Configuration -Arch $Arch
 
 exit $LASTEXITCODE
