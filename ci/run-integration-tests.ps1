b203ce9c72fb4d42e6c09c0c5e7b0613bbf22bb7
Maksym Kucherov <maximus.the0@gmail.com>
2023-10-31 15:41:49 +0200

    [TO BE REVERTED] Add tracing.

--- ci/run-integration-tests.ps1
+++ ci/run-integration-tests.ps1
@@ -10,6 +10,8 @@ param(
     [string]$OrgName
 )
 
+Set-PSDebug -Trace 2
+
 $NugetPackageFolder = [IO.Path]::Combine($pwd, "package")
 $EvidenceFiles = [IO.Path]::Combine($pwd, $RepoName,"FiftyOne.DeviceDetection.Hash.Engine.OnPremise", "device-detection-cxx", "device-detection-data")
 $ExamplesRepoName = "device-detection-dotnet-examples"
