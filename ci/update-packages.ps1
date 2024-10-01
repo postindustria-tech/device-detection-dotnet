8abc0f95f3201edb47749bbf6eaea0900e3bb3f5
Maksym Kucherov <maximus.the0@gmail.com>
2024-10-01 13:30:17 -0400

    Remove junk.

--- ci/update-packages.ps1
+++ ci/update-packages.ps1
@@ -5,17 +5,6 @@ param(
     [Parameter(Mandatory=$true)]
     [string]$RepoName
 )
-try {
-    ./dotnet/run-update-dependencies.ps1 -RepoName $RepoName -ProjectDir $ProjectDir -Name $Name -Debug
-}
-catch {
-    Write-Output "An error occurred:"
-    Write-Output $_
-}
-finally {
-    Write-Output "░░░░░░░░░ ░░░░░░░░░ ░░░░░░░░░ ░░░░░░░░░ ░░░░░░░░░"
-    Write-Output "In [update-packages.ps1]:"
-    Write-Output "LASTEXITCODE = $LASTEXITCODE"
-}
+./dotnet/run-update-dependencies.ps1 -RepoName $RepoName -ProjectDir $ProjectDir -Name $Name
 
 exit $LASTEXITCODE
